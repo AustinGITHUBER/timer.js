f39a4bb36ebc1469d8ff96b0f331c509dad165fa
AustinGITHUBER <73083603+AustinGITHUBER@users.noreply.github.com>
2022-12-09 11:08:30 +0800

    longer license

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 /*
-! timer.js © 2022 by AustinGITHUBER is licensed under CC BY 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/ 
+! timer.js © 2022 by AustinGITHUBER is licensed under Attribution 4.0 International. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
 */
 // uses strict mode
 'use strict'
